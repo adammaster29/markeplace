2a35f6f67b97646ce4c24da391873ce30496ba62
adammaster29 <104658788+adammaster29@users.noreply.github.com>
2023-07-15 16:22:57 -0500

    file/productos/navegacion

--- src/components/Productos.jsx
+++ src/components/Productos.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Productos = () => {
   const storedUsername = localStorage.getItem('username');
-
+const navigate = useNavigate()
+const login =()=>{
+    navigate("/login")
+}
   return (
     <div className='padre__productos'>
         {/* *****************navegacion************** */}
             <ul className="hijo__navbar-menu">
                 <li>  {storedUsername && <p>User: {storedUsername}</p>}</li>
-                <li><a href="/login">Login</a></li>
+                <li><span onClick={login} >Login</span></li>
                 <li><span>Categoria</span></li>
                 <li><span>Compras</span></li>
                 <li><span>Ventas</span></li>
@@ -16,7 +20,10 @@ const Productos = () => {
        
         {/* ******************productos********************** */}
         <div className="hijo__productos">
-
+            <div className="productos__title">
+                <h2>marketplace</h2>
+                <input type="text" name="buscar" id="buscar" />
+            </div>
         </div>
     </div>
   );
